831f0abd82fe3d8c1cb0c1473602d8c6b787649c
SRISARATHI V <158599725+SRISARATHI-V@users.noreply.github.com>
2024-06-21 10:42:12 +0530

    Create four_wires.v

---
+++ 2.Verilog_language/1.Basics/four_wires.v
@@ -0,0 +1,17 @@
+module top_module (
+	input a,
+	input b,
+	input c,
+	output w,
+	output x,
+	output y,
+	output z  );
+	
+	assign w = a;
+  assign y = b;
+	assign x = b;
+  assign z = c;
+
+	
+	
+endmodule
