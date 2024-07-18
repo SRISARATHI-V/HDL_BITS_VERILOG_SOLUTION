34e8c729ef6c91ed4185113066dd1952042eaa9a
SRISARATHI V <158599725+SRISARATHI-V@users.noreply.github.com>
2024-07-18 20:50:11 +0530

    Create shift register.v

---
+++ 3.CIRCUITS/sequential logic.v/shift registers/shift register.v
@@ -0,0 +1,11 @@
+module top_module (
+    input clk,
+    input resetn,   
+    input in,
+    output out);
+    reg [3:1] q;
+always @(posedge clk)
+    if (~resetn) {q, out} <= 0;
+    else {q, out} <= {in, q};
+
+endmodule
